c2209b46fda0e566fe0bb5fd432f842ea6f5c0c9
Егор Явна <edyavna@fortran.su>
2024-10-10 18:13:07 +0300

    Говорит привет

--- hello.pas
+++ hello.pas
@@ -1,1 +1,20 @@
-﻿
+﻿begin
+var lang := readinteger('1 - Русский язык, 2 - English, 3 - Español: ');
+assert((1 <= lang) and (3 >= lang));
+case lang of
+  1: print('Привет!');
+  2: print('Hello!');
+  3: print('Hola!');
+end;
+end.
+{log1:
+1 - Русский язык, 2 - English, 3 - Español: 3
+Hola! 
+
+log2:
+1 - Русский язык, 2 - English, 3 - Español: 1
+Привет!
+
+log3:
+1 - Русский язык, 2 - English, 3 - Español: 0 
+assert}
